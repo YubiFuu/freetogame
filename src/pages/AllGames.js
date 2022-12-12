49b135fde57234fc611a7aec4ecc89d9ffca6c3e
sidahmed2306 <sidahmedbc1@gmail.com>
2022-12-12 15:46:46 +0100

    drop dwon menue

--- src/pages/AllGames.js
+++ src/pages/AllGames.js
@@ -11,16 +11,49 @@ const AllGames = () => {
   console.log(data);
 
   return (
-    <main>
-      <div
-        className="image"
-        style={{
-          backgroundImage: `url(./images/league.png)`,
-        }}
-      >
-        <h2>All Games</h2>
-      </div>
-    </main>
+    <>
+      <header>
+        <div
+          className="image"
+          style={{
+            backgroundImage: `url(./images/league.png)`,
+          }}
+        >
+          <h2>All Games</h2>
+        </div>
+      </header>
+      <main>
+        <div className="filter">
+          <div class="dropdown">
+            <div class="dropbtn">
+              <h2>Platform</h2>
+              <svg
+                className="vector-down"
+                width="12"
+                height="17"
+                viewBox="0 0 12 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1 1L6 16L11 1"
+                  stroke="white"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+
+            <div class="dropdown-content">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
