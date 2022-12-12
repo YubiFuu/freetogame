6871fac8ebfa895476405c3338e08d48ffb13993
YubiFuu <jess.supercode@gmail.com>
2022-12-12 13:12:29 +0100

    überstehendes styling für img + überstehendes farbstyling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
 import "./Home.css";
 const Home = () => {
-    return <main></main>;
+    return (
+        <main>
+            <img
+                src="https://de.gamewallpapers.com/img_script/wallpaper_dir/img.php?src=wallpaper_league_of_legends_131_2560x1080.jpg&height=506&sharpen"
+                alt=""
+            />
+        </main>
+    );
 };
 
 export default Home;
