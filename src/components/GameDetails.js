8ba1b36d3df45a4f4fae5be741ef862e88d202d1
YubiFuu <jess.supercode@gmail.com>
2022-12-14 09:16:56 +0100

    details page added

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,20 +1,154 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { json, useParams } from "react-router-dom";
 import "./GameDetails.css";
 
 const GameDetails = () => {
-    const [gameDetail, setGameDetail] = useState([]);
     const { id } = useParams();
+    const [gameDetail, setGameDetail] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch(`https://www.freetogame.com/api/game?id=${id}`)
             .then((res) => res.json())
-            .then((json) => setGameDetail(json));
+            .then(
+                (json) => setGameDetail(json)
+                // setParagraph(json.description.split("."))
+            );
+        setLoading(false);
     }, []);
 
-    console.log(gameDetail);
+    if (loading) {
+        return (
+            <div>
+                <p>is loading...</p>
+            </div>
+        );
+    }
 
-    return <main></main>;
+    return (
+        <main className="details">
+            <div
+                className="image"
+                style={{
+                    backgroundImage: `url(${
+                        gameDetail.screenshots
+                            ? gameDetail.screenshots[0].image
+                            : null
+                    })`,
+                }}
+            ></div>
+
+            <h2>{gameDetail.title}</h2>
+            <div className="row-wrapper">
+                <div>
+                    <img
+                        className="thumbnail"
+                        src={gameDetail.thumbnail}
+                        alt="thumbnail of the game"
+                    />
+                    <h3>Platform: {gameDetail.platform}</h3>
+                    <p>{gameDetail.genre}</p>
+                    <a href={gameDetail.game_url}>Play Now</a>
+                </div>
+                <div>
+                    <h3>About</h3>
+                    <p>{gameDetail.description}</p>
+                </div>
+            </div>
+            <div className="row-wrapper">
+                <div>
+                    <img
+                        src={
+                            gameDetail.screenshots
+                                ? gameDetail.screenshots[1].image
+                                : null
+                        }
+                    />
+                    <h3>Additional Information</h3>
+                    <p>
+                        Please note this free-to-play game may or may not offer
+                        optional in-game purchases.
+                    </p>
+                    <div>
+                        <h5>Developer</h5>
+                        <p>{gameDetail.developer}</p>
+                    </div>
+                    <div>
+                        <h5>Publisher</h5>
+                        <p>{gameDetail.publisher}</p>
+                    </div>
+                    <div>
+                        <h5>Release Date</h5>
+                        <p>{gameDetail.release_date}</p>
+                    </div>
+                </div>
+                {gameDetail.minimum_system_requirements ? (
+                    <div>
+                        <img
+                            src={
+                                gameDetail.screenshots
+                                    ? gameDetail.screenshots[2].image
+                                    : null
+                            }
+                        />
+                        <h3>Minimum System Requirements</h3>
+                        <div>
+                            <div>
+                                <h5>OS</h5>
+                                <p>
+                                    {gameDetail.minimum_system_requirements.os}
+                                </p>
+                            </div>
+                            <div>
+                                <h5>Memory</h5>
+                                <p>
+                                    {
+                                        gameDetail.minimum_system_requirements
+                                            .memory
+                                    }
+                                </p>
+                            </div>
+                            <div>
+                                <h5>Storage</h5>
+                                <p>
+                                    {
+                                        gameDetail.minimum_system_requirements
+                                            .storage
+                                    }
+                                </p>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <h5>Processor</h5>
+                                <p>
+                                    {
+                                        gameDetail.minimum_system_requirements
+                                            .processor
+                                    }
+                                </p>
+                            </div>
+                            <div>
+                                <h5>Graphics</h5>
+                                <p>
+                                    {
+                                        gameDetail.minimum_system_requirements
+                                            .graphics
+                                    }
+                                </p>
+                            </div>
+                            <div>
+                                <h5>Additional Notes</h5>
+                                <p>
+                                    Specifications may change during development
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ) : null}
+            </div>
+        </main>
+    );
 };
 
 export default GameDetails;
