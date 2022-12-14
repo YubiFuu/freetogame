99d06ec957df13f5a53856193e34c440647e2c41
“Qadaar-TU” <qadaar@gmail.com>
2022-12-14 13:14:17 +0100

    recently page done

--- src/pages/RecentlyAdded.js
+++ src/pages/RecentlyAdded.js
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
+import GameCard from "../components/GameCard.js";
 import "./RecentlyAdded.css";
 const RecentlyAdded = () => {
-    return <main></main>;
+  const [recentGames, setRecentGames] = useState([]);
+  useEffect(() => {
+    fetch("https://www.freetogame.com/api/games?sort-by=release-date")
+      .then((res) => res.json())
+      .then((json) => setRecentGames(json));
+  }, []);
+
+  return (
+    <>
+      <header>
+        <div
+          id="img"
+          className="image"
+          style={{
+            backgroundImage: `url(./images/league.png)`,
+          }}
+        >
+          <h2>Recently Added</h2>
+        </div>
+      </header>
+      <main>
+        <div className="gridd">
+          {recentGames.map((elt) => (
+            <GameCard key={elt.id} {...elt} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default RecentlyAdded;
