62904906cd7db4db8815e93bef7abac1665dbef6
YubiFuu <jess.supercode@gmail.com>
2022-12-14 14:29:48 +0100

    styling verfeinert

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,23 @@ import { Link } from "react-router-dom";
 import GameCard from "../components/GameCard";
 import AppContext from "../components/AppContext";
 import "./Home.css";
+import setBackground from "../components/setBackground";
+
 const Home = () => {
     const gameName = useContext(AppContext);
-    console.log(gameName.nameContext);
     const [recentGames, setRecentGames] = useState([]);
     const [topPc, setTopPc] = useState([]);
     const [topBrowser, setTopBrowser] = useState([]);
+    const [loading1, setLoading1] = useState(true);
+    const [loading2, setLoading2] = useState(true);
+    const [loading3, setLoading3] = useState(true);
+    const [pcFirstBackground, setPcFirstBackground] = useState("");
 
     useEffect(() => {
         fetch("https://www.freetogame.com/api/games?sort-by=release-date")
             .then((res) => res.json())
             .then((json) => setRecentGames(json));
+        setLoading1(false);
     }, []);
 
     useEffect(() => {
@@ -22,6 +28,7 @@ const Home = () => {
         )
             .then((res) => res.json())
             .then((json) => setTopPc(json));
+        setLoading2(false);
     }, []);
     useEffect(() => {
         fetch(
@@ -29,14 +36,31 @@ const Home = () => {
         )
             .then((res) => res.json())
             .then((json) => setTopBrowser(json));
+        setLoading3(false);
     }, []);
+
+    if (loading1 || loading2 || loading3) {
+        return <p>is loading...</p>;
+    }
+
+    if (topPc.length > 0) {
+        fetch(`https://www.freetogame.com/api/game?id=${topPc[0].id}`)
+            .then((res) => res.json())
+            .then((json) => setPcFirstBackground(json.screenshots[0].image));
+    }
+
+    if (topPc.length > 0) {
+        setBackground({
+            image: `url(${pcFirstBackground})`,
+        });
+    }
 
     return (
         <main className="home">
             <div
                 className="image"
                 style={{
-                    backgroundImage: `url(https://de.gamewallpapers.com/img_script/wallpaper_dir/img.php?src=wallpaper_league_of_legends_131_2560x1080.jpg&height=506&sharpen)`,
+                    backgroundImage: `url(./images/LeagueOfLegends.png)`,
                 }}
             >
                 <h2>Find & Track the best Free-To-Play Games!</h2>
@@ -53,8 +77,8 @@ const Home = () => {
             <div>
                 <h3>Top 4 Games for PC</h3>
                 <div className="toppc-grid">
-                    {topPc.slice(0, 4).map((e) => (
-                        <GameCard key={e.id} {...e} />
+                    {topPc.slice(0, 4).map((e, index) => (
+                        <GameCard igitt={index + 1} key={e.id} {...e} />
                     ))}
                 </div>
                 <Link to="/games">Show More</Link>
